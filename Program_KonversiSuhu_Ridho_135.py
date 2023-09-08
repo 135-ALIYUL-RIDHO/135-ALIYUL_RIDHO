1996ed32a012a72eb296b97b946c8b1b6290af17
135-ALIYUL-RIDHO <144401652+135-ALIYUL-RIDHO@users.noreply.github.com>
2023-09-08 08:28:28 +0700

    Add files via upload

---
+++ Program_KonversiSuhu_Ridho_135.py
@@ -0,0 +1,19 @@
+# mengkonversi suhu Celcius ke Fahrenheit
+
+suhu = int(input("Masukan Nilai Suhu Celcius Yang Anda Ingin Konversi Menjadi Farenheit :"))
+conv_suhu = 9 * suhu / 5+32
+
+print('----------------------------------------------------------------')
+print(f'Hasil Konversi Suhu {suhu} Celcius adalah {conv_suhu} farenheit')
+print('----------------------------------------------------------------')
+
+
+
+
+# mengkonversi suhu Kelvin menjadi Celcius
+
+Kelvin = int(input('Masukan Nilai Suhu Kelvin Yang Ingin Anda Konversi Ke Celcius :'))
+Konv_suhu = Kelvin - 273
+print('----------------------------------------------------------------')
+print(f'Hasil Konversi Suhu {Kelvin} Kelvin adalah {Konv_suhu} Celcius')
+print('----------------------------------------------------------------')
